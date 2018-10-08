4723672ed18dd3628d07d8e84eac1d6c939179f9
김민영 <mycielo91@gmail.com>
2018-10-08 18:33:07 +0900

    project initialization

---
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelCtrl : MonoBehaviour {
+
+    public GameObject expEffect;
+    public Mesh[] meshes;
+    public Texture[] textures;
+
+    private int hitCount = 0;
+    private Rigidbody rb;
+    private MeshFilter meshFilter;
+    private MeshRenderer _renderer;
+
+    public float expRadius = 10.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        meshFilter = GetComponent<MeshFilter>();
+        _renderer = GetComponent<MeshRenderer>();
+        _renderer.material.mainTexture = textures[Random.Range(0, textures.Length)];
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("BULLET"))
+        {
+            if (++hitCount == 3)
+            {
+                ExpBarrel();
+            }
+        }
+    }
+
+    void ExpBarrel()
+    {
+        GameObject effect = Instantiate(expEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 2.0f);
+        rb.mass = 1.0f;
+        //rb.AddForce(Vector3.up * 1000.0f);
+        IndirectDamage(transform.position);
+
+        int idx = Random.Range(0, meshes.Length);
+        meshFilter.sharedMesh = meshes[idx];
+        GetComponent<MeshCollider>().sharedMesh = meshes[idx];
+    }
+
+    void IndirectDamage(Vector3 pos)
+    {
+        Collider[] colls = Physics.OverlapSphere(pos, expRadius, 1 << 8);
+
+        foreach(var coll in colls)
+        {
+            var _rb = coll.GetComponent<Rigidbody>();
+            _rb.mass = 1.0f;
+            _rb.AddExplosionForce(1200.0f, pos, expRadius, 1000.0f);
+        }
+    }
+}
---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerSfx
+{
+    public AudioClip[] fire;
+    public AudioClip[] reload;
+}
+
+public class FireCtrl : MonoBehaviour {
+
+    public enum WeaponType
+    {
+        RIFLE = 0,
+        SHOTGUN
+    }
+
+    public WeaponType currWeapon = WeaponType.RIFLE;
+
+    public GameObject bullet;
+    public ParticleSystem cartridge;
+    private ParticleSystem muzzleFlash;
+    private AudioSource _audio;
+    public Transform firePos;
+    public PlayerSfx playerSfx;
+
+    // Use this for initialization
+    void Start () {
+        muzzleFlash = firePos.GetComponentInChildren<ParticleSystem>();
+        _audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+	}
+
+    void Fire()
+    {
+        Instantiate(bullet, firePos.position, firePos.rotation);
+        cartridge.Play();
+        muzzleFlash.Play();
+        FireSfx();
+    }
+
+    void FireSfx()
+    {
+        var _sfx = playerSfx.fire[(int)currWeapon];
+        _audio.PlayOneShot(_sfx, 1.0f);
+    }
+
+}
